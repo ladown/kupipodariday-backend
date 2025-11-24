fc77fa8f7878d8433f8c852e132df9b0490131fa
Egor Ermilov <egorka33rus52@gmail.com>
2025-11-24 16:31:34 +0300

    feat: improve app initialize

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
-import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+
+import { GlobalExceptionFilter } from './common/filters/global-exception.filter';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  const httpAdapterHost = app.get(HttpAdapterHost);
+
+  app.useGlobalFilters(new GlobalExceptionFilter(httpAdapterHost));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      skipMissingProperties: true,
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
